6cfc3d8ab42b1714a2cb91ee902d8b39d4fe5689
bryopsida <8363252+bryopsida@users.noreply.github.com>
2025-11-27 08:34:17 -0600

    use lavamoat allow-scripts

--- test-content/nest-project/src/app.module.ts
+++ test-content/nest-project/src/app.module.ts
@@ -1,9 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'better-sqlite3',
+      database: 'db.sqlite',
+      synchronize: true,
+      entities: [User],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
